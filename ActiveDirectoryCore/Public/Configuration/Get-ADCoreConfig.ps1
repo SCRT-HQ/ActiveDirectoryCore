f0df946ea6aeabdb22070d0b82614d1fe859b5ad
Nate Ferrell <nate@scrthq.com>
2020-04-04 14:54:17 -0500

    saving progress, switching machines

---
+++ ActiveDirectoryCore/Public/Configuration/Get-ADCoreConfig.ps1
@@ -0,0 +1,95 @@
+function Get-ADCoreConfig {
+    <#
+    .SYNOPSIS
+    Loads the specified ADCore config
+
+    .DESCRIPTION
+    Loads the specified ADCore config
+
+    .PARAMETER ConfigName
+    The config name to load
+
+    .PARAMETER Path
+    The path of the config to load if non-default.
+
+    This can be used to load either a legacy XML config from an older version of ADCore or a specific .PSD1 config created with version 2.0.0 or greater
+
+    .PARAMETER Scope
+    The config scope to load
+
+    .PARAMETER PassThru
+    If specified, returns the config after loading it
+
+    .PARAMETER NoImport
+    If $true, just returns the specified config but does not impart it in the current session.
+
+    .EXAMPLE
+    Get-ADCoreConfig personalDomain -PassThru
+
+    This will load the config named "personalDomain" and return it as a PSObject.
+    #>
+    [cmdletbinding(DefaultParameterSetName = "ConfigurationModule")]
+    Param
+    (
+        [parameter(Mandatory = $false,Position = 0,ParameterSetName = "ConfigurationModule")]
+        [String]
+        $ConfigName,
+        [Parameter(Mandatory = $false,ParameterSetName = "Path")]
+        [ValidateScript( {Test-Path $_})]
+        [String]
+        $Path,
+        [Parameter(Mandatory = $false,Position = 1)]
+        [ValidateSet("User", "Machine", "Enterprise", $null)]
+        [string]
+        $Scope = $Script:ConfigScope,
+        [Parameter(Mandatory = $false)]
+        [Switch]
+        $PassThru,
+        [Parameter(Mandatory = $false)]
+        [Switch]
+        $NoImport
+    )
+    Process {
+        $script:ConfigScope = $Scope
+        switch ($PSCmdlet.ParameterSetName) {
+            ConfigurationModule {
+                $fullConf = Import-SpecificConfiguration -CompanyName 'SCRT HQ' -Name 'ActiveDirectoryCore' -Scope $Script:ConfigScope
+                if (!$ConfigName) {
+                    $choice = $fullConf["DefaultConfig"]
+                    Write-Verbose "Importing default config: $choice"
+                }
+                else {
+                    $choice = $ConfigName
+                    Write-Verbose "Importing config: $choice"
+                }
+                $encConf = [PSCustomObject]($fullConf[$choice])
+            }
+            Path {
+                $encConf = switch ((Get-Item -Path $Path).Extension) {
+                    '.xml' {
+                        Import-Clixml -Path $Path
+                        $choice = "LegacyXML"
+                    }
+                    '.psd1' {
+                        Import-SpecificConfiguration -Path $Path
+                        $choice = "CustomConfigurationFile"
+                    }
+                }
+            }
+        }
+        $decryptParams = @{
+            ConfigName = $choice
+        }
+        if ($Path) {
+            $decryptParams['ConfigPath'] = $Path
+        }
+        $decryptedConfig = $encConf | Get-GSDecryptedConfig @decryptParams
+        Write-Verbose "Retrieved configuration '$choice'"
+        if (!$NoImport) {
+            $Script:ActiveDirectoryCore = $decryptedConfig
+        }
+        if ($PassThru) {
+            $decryptedConfig
+        }
+    }
+}
---
+++ ActiveDirectoryCore/Public/Configuration/Switch-ADCoreConfig.ps1
@@ -0,0 +1,100 @@
+﻿function Switch-ADCoreConfig {
+    <#
+    .SYNOPSIS
+    Switches the active config
+
+    .DESCRIPTION
+    Switches the active config
+
+    .PARAMETER ConfigName
+    The friendly name of the config you would like to set as active for the session
+
+    .PARAMETER Domain
+    The domain name for the config you would like to set as active for the session
+
+    .PARAMETER SetToDefault
+    If passed, also sets the specified config as the default so it's loaded on the next module import
+
+    .EXAMPLE
+    Switch-ADCoreConfig newCustomer
+
+    Switches the config to the "newCustomer" config
+    #>
+    [CmdletBinding(DefaultParameterSetName = "ConfigName",PositionalBinding = $false)]
+    Param (
+        [parameter(Mandatory = $true,ParameterSetName = "Domain")]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Domain,
+        [parameter(Mandatory = $false)]
+        [switch]
+        $SetToDefault
+    )
+    DynamicParam {
+        $attribute = New-Object System.Management.Automation.ParameterAttribute
+        $attribute.Position = 0
+        $attribute.Mandatory = $true
+        $attribute.ParameterSetName = "ConfigName"
+        $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+        $attributeCollection.Add($attribute)
+        $names = Get-ADCoreConfigNames -Verbose:$false
+        $attributeCollection.Add((New-Object  System.Management.Automation.ValidateSetAttribute($names)))
+        $parameter = New-Object System.Management.Automation.RuntimeDefinedParameter('ConfigName', [String], $attributeCollection)
+        $paramDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+        $paramDictionary.Add('ConfigName', $parameter)
+        return $paramDictionary
+    }
+    Process {
+        $ConfigName = $PSBoundParameters['ConfigName']
+        if ($Script:ActiveDirectoryCore.Domain -eq $Domain) {
+            Write-Verbose "Current config is already set to domain '$Domain' --- retaining current config. If you would like to import a different config for the same domain, please use the -ConfigName parameter instead"
+            if ($SetToDefault) {
+                Write-Verbose "Setting config name '$($Script:ActiveDirectoryCore.ConfigName)' for domain '$($Script:ActiveDirectoryCore.Domain)' as default"
+                Set-ADCoreConfig -ConfigName $($Script:ActiveDirectoryCore.ConfigName) -SetAsDefaultConfig -Verbose:$false
+            }
+        }
+        elseif ($Script:ActiveDirectoryCore.ConfigName -eq $ConfigName) {
+            Write-Verbose "Current config is already set to '$ConfigName' --- retaining current config"
+            if ($SetToDefault) {
+                Write-Verbose "Setting config name '$($Script:ActiveDirectoryCore.ConfigName)' for domain '$($Script:ActiveDirectoryCore.Domain)' as default"
+                Set-ADCoreConfig -ConfigName $($Script:ActiveDirectoryCore.ConfigName) -SetAsDefaultConfig -Verbose:$false
+            }
+        }
+        else {
+            $fullConf = Import-SpecificConfiguration -CompanyName 'SCRT HQ' -Name 'ActiveDirectoryCore' -Scope $Script:ConfigScope -Verbose:$false
+            $defaultConfigName = $fullConf['DefaultConfig']
+            $choice = switch ($PSCmdlet.ParameterSetName) {
+                Domain {
+                    Write-Verbose "Switching active domain to '$Domain'"
+                    $fullConf.Keys | Where-Object {(Decrypt $fullConf[$_]['Domain']) -eq $Domain}
+                }
+                ConfigName {
+                    Write-Verbose "Switching active config to '$ConfigName'"
+                    $fullConf.Keys | Where-Object {$_ -eq $ConfigName}
+                }
+            }
+            if ($choice) {
+                $Script:ActiveDirectoryCore = [PSCustomObject]($fullConf[$choice]) | Get-GSDecryptedConfig -ConfigName $choice
+                if ($SetToDefault) {
+                    if ($defaultConfigName -ne $choice) {
+                        Write-Verbose "Setting config name '$choice' for domain '$($Script:ActiveDirectoryCore.Domain)' as default"
+                        Set-ADCoreConfig -ConfigName $choice -SetAsDefaultConfig -Verbose:$false
+                    }
+                    else {
+                        Write-Warning "Config name '$choice' for domain '$($Script:ActiveDirectoryCore.Domain)' is already set to default --- no action taken"
+                    }
+                }
+            }
+            else {
+                switch ($PSCmdlet.ParameterSetName) {
+                    Domain {
+                        Write-Warning "No config found for domain '$Domain'! Retaining existing config for domain '$($Script:ActiveDirectoryCore.Domain)'"
+                    }
+                    ConfigName {
+                        Write-Warning "No config named '$ConfigName' found! Retaining existing config '$($Script:ActiveDirectoryCore.ConfigName)'"
+                    }
+                }
+            }
+        }
+    }
+}
